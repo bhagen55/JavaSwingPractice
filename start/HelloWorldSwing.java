9e8a26f67775e2d98b7e815c2a5c10b8cba67aa7
Blair Hagen <hagenb@union.edu>
2018-02-02 17:25:38 -0500

    Added a simple hello world

---
+++ start/HelloWorldSwing.java
@@ -0,0 +1,30 @@
+package start;
+
+import javax.swing.*;
+
+public class HelloWorldSwing {
+
+  private static void createAndShowGUI()
+  {
+    JFrame frame = new JFrame("HelloWorldSwing");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    JLabel label = new JLabel("Fuck you world");
+    frame.getContentPane().add(label);
+
+    frame.pack();
+    frame.setVisible(true);
+  }
+
+  public static void main(String[] args)
+  {
+    javax.swing.SwingUtilities.invokeLater(new Runnable()
+    {
+      public void run()
+      {
+        createAndShowGUI();
+      }
+
+    });
+  }
+}
